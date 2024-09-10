ae4c801e7bf3a6bb5f915dff33f1c3274728510c
01pieV <jamriska.janko267@gmail.com>
2024-09-10 09:32:32 +0200

    pozn

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Cau")
+x = 10
+x = x + 2 # x+=2
+x = x * 2 # x*=2
+# x = x - 2 # x-=2
+# = - vloz
+# == - normalne =
+y = x % 2 # modulo zvyšok po delení
+# print(20%3) # vysledok je 2
+z = x // 2 # celociselne delenie
+# print(20 // 3) vysledok je 6
+#trik
+# 124 % 10 = posledne cislo - vysledok 4
+# 124 // 10 = prve 2 cisla - vysledok 12
+# if x % 2 == 0 -- parne
+# if x % 2! = 0
+# if x % 2 == 1 (0,1)
+print(type(x))
