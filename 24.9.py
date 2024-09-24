76b4e661c7f0bd4129fee4f008db16a8167ef8a1
01pieV <jamriska.janko267@gmail.com>
2024-09-24 09:12:18 +0200

    Update 24.9.py

--- 24.9.py
+++ 24.9.py
@@ -5,4 +5,18 @@ def schreder ():
         if prve_dve_c ** 2 + posl_dve_c ** 2 == i:
             print(i)
 
-schreder()
+#spocitanie cisel
+def schreder2():
+    tog = 0
+    for i in range(1000,10000):
+        tog = tog + i # tog += 1
+    return tog
+
+#faktorial
+def fak(a):
+    tog = 1
+    for i in range(1, a+1):
+        tog = tog * i
+    return tog
+
+print(fak(5))
